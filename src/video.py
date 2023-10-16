908f2fb9a5780812bee268d11aeea8b06b7551c8
Arina <arinafedor23@icloud.com>
2023-10-16 17:29:40 +0300

    add except in video.py

--- src/video.py
+++ src/video.py
@@ -1,5 +1,6 @@
 from googleapiclient.discovery import build
 from src.config import get_api_key
+from googleapiclient.errors import HttpError
 
 
 class Video:
@@ -9,24 +10,29 @@ class Video:
 
     def __init__(self, id_video):
         self.id_video = id_video
-        self.name_video = ""
-        self.url_video = ""
+        self.name_video = None
+        self.url_video = None
         self.view_video = None
         self.like_video = None
 
-        self.request = self.get_service().videos().list(
-            part="snippet,statistics",
-            id=self.id_video
-        )
 
-        response = self.request.execute()
+        try:
+            self.request = self.get_service().videos().list(
+                part="snippet,statistics",
+                id=self.id_video
+            )
 
-        if response['items']:
-            video_data = response['items'][0]
-            self.name_video = video_data['snippet']['title']
-            self.url = f"https://www.youtube.com/watch?v={self.id_video}"
-            self.view_video = int(video_data['statistics']['viewCount'])
-            self.like_video = int(video_data['statistics']['likeCount'])
+            response = self.request.execute()
+
+            if response['items']:
+                video_data = response['items'][0]
+                self.name_video = video_data['snippet']['title']
+                self.url = f"https://www.youtube.com/watch?v={self.id_video}"
+                self.view_video = int(video_data['statistics']['viewCount'])
+                self.like_video = int(video_data['statistics']['likeCount'])
+        except HttpError as e:
+            # В случае ошибки при запросе API, устанавливаем атрибуты в None
+            print(f"Ошибка при получении данных для видео {self.id_video}: {e}")
 
     def get_service(self):
         return build("youtube", "v3", developerKey=self.api_key)
