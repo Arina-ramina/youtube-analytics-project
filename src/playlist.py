fd4534de4cf86c017e01b89eff6908b77132eba0
Arina <arinafedor23@icloud.com>
2023-10-16 13:21:38 +0300

    wait Renat

--- src/playlist.py
+++ src/playlist.py
@@ -1,11 +1,9 @@
 import datetime
-
 import isodate
-
-import channel
 from src.video import PLVideo
 from src.config import get_api_key
 from googleapiclient.discovery import build
+
 
 class PlayList(PLVideo):
     def __init__(self, plist_id):
@@ -14,19 +12,17 @@ class PlayList(PLVideo):
         self.videos = []
         self.title = None
         self.url = None
-        self.load_playlist_info()  # Метод для загрузки информации о плейлисте
+        self.api_key = get_api_key()  # Получаем API ключ
+        self.video_response = None  # Инициализируем переменную
 
-        self.video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.load_playlist_info['items']]
+        # Загрузка информации о плейлисте
+        self.load_playlist_info()
 
-
-        self.video_response = PlayList.get_service().videos().list(part='contentDetails,statistics',
-                                                                   id=','.join(self.video_ids)
-                                                                   ).execute()
-
+    def get_service(self):
+        return build("youtube", "v3", developerKey=self.api_key)
 
     def load_playlist_info(self):
-        api_key = get_api_key()
-        youtube = build('youtube', 'v3', developerKey=api_key)
+        youtube = self.get_service()
         playlist_info = youtube.playlists().list(part='snippet', id=self.plist_id).execute()
 
         if 'items' in playlist_info:
@@ -34,30 +30,40 @@ class PlayList(PLVideo):
             self.title = playlist_data['snippet']['title']
             self.url = f"https://www.youtube.com/playlist?list={self.plist_id}"
 
-    def add_video(self, video):
-        self.videos.append(video)
+    def get_video_ids(self):
+        # Получаем список ID видеороликов в плейлисте
+        if self.video_response is not None and 'items' in self.video_response:
+            return [video['contentDetails']['videoId'] for video in self.video_response['items']]
+        return []
 
-    def remove_video(self, video):
-        self.videos.remove(video)
+    def update_video_info(self):
+        # Получаем информацию о видеороликах в плейлисте и обновляем video_response
+        youtube = self.get_service()
+        video_ids = self.get_video_ids()
+        video_response = youtube.videos().list(part='contentDetails,statistics',
+                                               id=','.join(video_ids)
+                                               ).execute()
+        self.video_response = video_response
 
     @property
     def total_duration(self):
-        self.video_response = channel.Channel.get_service().videos().list(part='contentDetails', id=', '.join(self.plist_id)).execute()
-        print(self.video_response)
-        duration = datetime.timedelta()
-        for video in self.video_response['items']:
-            iso_8601_duration = video['contentDetails']['duration']
-            duration += isodate.parse_duration(iso_8601_duration)
-        return duration
-
-        # total_duration_seconds = sum(video.duration.total_seconds() for video in video_response['items'])
-        # return datetime.timedelta(seconds=total_duration_seconds)
+        # Получаем общую длительность видеороликов в плейлисте
+        if self.video_response is not None and 'items' in self.video_response:
+            duration = datetime.timedelta()
+            for video in video_response['items']:
+                # YouTube video duration is in ISO 8601 format
+                iso_8601_duration = video['contentDetails']['duration']
+                duration = isodate.parse_duration(iso_8601_duration)
+                print(duration)
+        return datetime.timedelta()
 
     def show_best_video(self):
         best_video = max(self.videos, key=lambda video: video.like_video)
         return best_video.url
 
+
 pl = PlayList('PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw')
 duration = pl.total_duration
-print(str(duration)) #== "1:49:52"
+print(duration)
+
 
